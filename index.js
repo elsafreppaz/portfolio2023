0c3178b011913a4c70f9be53dd9d03b2952bfda4
studiomeraki <itsstudiomeraki@gmail.com>
2023-12-07 13:04:42 +0100

    Burger Menu Animation

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 function openOuverture () {
     document.querySelector(".ouverture").style.display="block";
-    document.querySelector("main").style.display="none";
-    document.querySelector("footer").style.display="none";
+    document.querySelector(".ouverture").style.animation="ouverture 500ms"
+    setTimeout(() => {
+        document.querySelector("main").style.display="none";
+        document.querySelector("footer").style.display="none";
+    }, 500);
+
+    
 }
 
 function closeOuverture () {
-    document.querySelector(".ouverture").style.display="none";
+    document.querySelector(".ouverture").style.animation="fermeture 500ms"
+    setTimeout(() => {
+        document.querySelector(".ouverture").style.display="none";
+    }, 500);
     document.querySelector("main").style.display="block";
     document.querySelector("footer").style.display="block";
 }
+
+
 
