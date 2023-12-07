4eb272e7fb544222b789ab7e3352af73b513f393
studiomeraki <itsstudiomeraki@gmail.com>
2023-12-07 12:36:25 +0100

    Burger Menu

---
+++ index.js
@@ -0,0 +1,12 @@
+function openOuverture () {
+    document.querySelector(".ouverture").style.display="block";
+    document.querySelector("main").style.display="none";
+    document.querySelector("footer").style.display="none";
+}
+
+function closeOuverture () {
+    document.querySelector(".ouverture").style.display="none";
+    document.querySelector("main").style.display="block";
+    document.querySelector("footer").style.display="block";
+}
+
